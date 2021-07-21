bdad2ecb9a29f404f7e6c3c3ec7f672324ed1640
alsousha <47866010+alsousha@users.noreply.github.com>
2021-07-21 15:28:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 document.querySelectorAll('.tabs-wrapper').forEach((e) => {
     let tabTabs = e.querySelectorAll('.tabs .tab');
     let tabItems = e.querySelectorAll('.tabs-items .item');
-      for(let i =0;i<tabTabs.length;i++) {
-          tabTabs[0].click();
-          tabTabs[i].onclick = () => {
-          tabTabs.forEach((e)  => { e.classList.remove('on') }); 
-          tabItems.forEach((e)  => { e.classList.remove('on') });
-          tabTabs[i].classList.add('on');
-          tabItems[i].classList.add('on');
-       }
+    
+//       new syntax
+//  for(let i =0;i<tabTabs.length;i++) {
+//           tabTabs[0].click();
+//           tabTabs[i].onclick = () => {
+//           tabTabs.forEach((e)  => { e.classList.remove('on') }); 
+//           tabItems.forEach((e)  => { e.classList.remove('on') });
+//           tabTabs[i].classList.add('on');
+//           tabItems[i].classList.add('on');
+//        }
+    
+       for (let i = 0; i < tabTabs.length; i++) {
+           tabTabs[0].click(); //add function onclick (below) for all first tab
+           
+           //for all tabs assign (вешаем) function that removes class ON from all tabs
+           tabTabs[i].onclick = function() {
+                tabTabs.forEach(function(e) {
+                    e.classList.remove('on') 
+                });
+                tabItems.forEach(function(e){
+                    e.classList.remove('on') 
+                });
+               
+               //for tab that was clicked add class ON
+                tabTabs[i].classList.add('on');
+                tabItems[i].classList.add('on');
+            }
+        }
      }
    });
