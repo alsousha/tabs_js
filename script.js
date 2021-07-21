43a86f91bbaf52eba8db47348bcbe92e02dc2506
alsousha <47866010+alsousha@users.noreply.github.com>
2021-07-21 15:40:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ document.querySelectorAll('.tabs-wrapper').forEach((e) => {
 //  for(let i =0;i<tabTabs.length;i++) {
 //           tabTabs[0].click();
 //           tabTabs[i].onclick = () => {
-//           tabTabs.forEach((e)  => { e.classList.remove('on') }); 
-//           tabItems.forEach((e)  => { e.classList.remove('on') });
-//           tabTabs[i].classList.add('on');
-//           tabItems[i].classList.add('on');
-//        }
+    //           tabTabs.forEach((e)  => { e.classList.remove('on') }); 
+    //           tabItems.forEach((e)  => { e.classList.remove('on') });
+    //           tabTabs[i].classList.add('on');
+    //           tabItems[i].classList.add('on');
+    //        }
+//   }
     
        for (let i = 0; i < tabTabs.length; i++) {
            tabTabs[0].click(); //add function onclick (below) for all first tab
@@ -29,5 +30,5 @@ document.querySelectorAll('.tabs-wrapper').forEach((e) => {
                 tabItems[i].classList.add('on');
             }
         }
-     }
+     
    });
